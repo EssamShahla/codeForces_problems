d9eb68a8a3a7a563c1aa7df192b1d1d85656dc9a
Essam Shahla <122321545+EssamShahla@users.noreply.github.com>
2023-02-26 21:41:11 +0200

    Update problem8.java

--- Int or Float/problem8.java
+++ Int or Float/problem8.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class Proble {	
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("enter 3 numbers to be sorted Acs =>");
-        Double num = input.nextDouble();
+		System.out.println("enter a number to check it if it is float or int  =>");
+        double num = input.nextDouble();
         if (Math.floor(num) == num) {
             // N is an integer
         	int numInt =num.intValue();
